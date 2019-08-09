d0b55df014b22db1ffe6c306bdc26cc2f2320fcf
adishthapa <adishthapa@outlook.com>
2019-08-09 18:42:03 -0400

    Finished implementing all of essential process for app

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use("/css", express.static(__dirname +"/app/public/css"));
 app.use("/images", express.static(__dirname + "/app/public/images"));
 app.use("/js", express.static(__dirname + "/app/public/js"));
 
-require(path.join(__dirname, '/app/routing/htmlRoutes'))(app);
 require(path.join(__dirname, '/app/routing/apiRoutes'))(app);
+require(path.join(__dirname, '/app/routing/htmlRoutes'))(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
